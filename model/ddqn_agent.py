bcc5a8879c0ebf1f85ed7b89a7fc76ba169a85e5
Xinmin Du <10803082+doris-xm@user.noreply.gitee.com>
2024-04-18 09:51:12 +0800

    change to dqn

--- model/ddqn_agent.py
+++ model/ddqn_agent.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+from torch.nn import functional as F
 import gymnasium as gym
 from stable_baselines3 import DQN
 from stable_baselines3.common.evaluation import evaluate_policy
@@ -25,9 +26,47 @@ class DDQN_agent:
         self.model = DQN("MlpPolicy", (32,), action_space, verbose=1,
                          learning_rate=learning_rate, buffer_size=10000, gamma=gamma)
 
-    def train(self, iteration_times):
-        self.model.learn(total_timesteps=iteration_times)
+    def train(self, gradient_steps: int, batch_size: int = 100) -> None:
+        # Switch to train mode (this affects batch norm / dropout)
+        self.model.policy.set_training_mode(True)
+        # Update learning rate according to schedule
+        self.model._update_learning_rate(self.model.policy.optimizer)
 
-        mean_reward, std_reward = evaluate_policy(self.model, self.env, n_eval_episodes=10, render=False)
-        self.env.close()
-        return mean_reward, std_reward
+        losses = []
+        for _ in range(gradient_steps):
+
+            # Sample replay buffer
+            replay_data = self.replay_buffer.sample(batch_size, env=self._vec_normalize_env)  # type: ignore[union-attr]
+
+            with torch.no_grad():
+                # Compute the next Q-values using the target network
+                next_q_values = self.model.q_net_target(replay_data.next_observations)
+                # Follow greedy policy: use the one with the highest value
+                next_q_values, _ = next_q_values.max(dim=1)
+                # Avoid potential broadcast issue
+                next_q_values = next_q_values.reshape(-1, 1)
+                # 1-step TD target
+                target_q_values = replay_data.rewards + (1 - replay_data.dones) * self.gamma * next_q_values
+
+            # Get current Q-values estimates
+            current_q_values = self.model.q_net(replay_data.observations)
+
+            # Retrieve the q-values for the actions from the replay buffer
+            current_q_values = torch.gather(current_q_values, dim=1, index=replay_data.actions.long())
+
+            # Compute Huber loss (less sensitive to outliers)
+            loss = F.smooth_l1_loss(current_q_values, target_q_values)
+            losses.append(loss.item())
+
+            # Optimize the policy
+            self.model.policy.optimizer.zero_grad()
+            loss.backward()
+            # Clip gradient norm
+            torch.nn.utils.clip_grad_norm_(self.model.policy.parameters(), self.model.max_grad_norm)
+            self.model.policy.optimizer.step()
+
+        # Increase update counter
+        self.model._n_updates += gradient_steps
+
+        self.model.logger.record("train/n_updates", self.model._n_updates, exclude="tensorboard")
+        self.model.logger.record("train/loss", np.mean(losses))
